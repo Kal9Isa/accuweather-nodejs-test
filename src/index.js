f7e64b8b7794120157e437d7a40c03772de87ebe
kal9isa <docalisa_it@yahoo.com>
2021-08-17 22:56:51 +0430

    objectives added

---
+++ src/index.js
@@ -0,0 +1,8 @@
+// TODO Get URL from user
+// TODO Get current time to find day of month
+// TODO Send req to URL
+// TODO Find End of Month - EoM index
+// TODO Diff the EoM from current day
+// TODO Send req for each day (*run multi-threaded)
+// TODO Extract content
+// TODO Save Object in a JSON file
