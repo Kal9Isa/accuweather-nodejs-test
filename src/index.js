a25e72d630fe59ffec9e910efd636fa91d2dc69d
kal9isa <docalisa_it@yahoo.com>
2021-08-18 23:04:03 +0430

    save to JSON done

--- src/index.js
+++ src/index.js
@@ -1,4 +1,5 @@
 const axios = require('axios');
+const fs = require('fs');
 const { dayParser } = require('./services/day-parser');
 const { findEoM } = require('./services/find-eom');
 const { monthParser } = require('./services/month-parser');
@@ -8,19 +9,16 @@ const weatherSrcURL =
   process.argv[2] != undefined
     ? process.argv[2]
     : 'https://www.accuweather.com/en/gb/london/ec4a-2/august-weather/328328';
-// https://www.accuweather.com/en/gb/london/ec4a-2/daily-weather-forecast/328328?day=1
 
-// TODO Get current time to find day of month
+// Get current time to find day of month
 const today = new Date().getDate();
 // Can also find using "is-today" class on the elements after request
-
-// TODO Send req to URL & fix a list of urls
-// TODO Refactor
 
 const sendAllReqs = async (links, diff) => {
   let reqs = [];
   for (let index = 0; index <= diff; index++) {
     reqs.push(
+      // https://www.accuweather.com/en/gb/london/ec4a-2/daily-weather-forecast/328328?day=1
       await axios(`https://www.accuweather.com/${links[index]}`)
         .then((response) => dayParser(response))
         .catch(console.error)
@@ -46,13 +44,17 @@ const main = async () => {
       let lastDay = findEoM(response);
       let diffDaysCount = lastDay - today;
       let final = await sendAllReqs(dayLinks, diffDaysCount);
-      console.log(arrToJSON(final));
+      fs.writeFile('daily-forecast.json', arrToJSON(final), (err) => {
+        if (err) {
+          throw err;
+        }
+        console.log('JSON data is saved.');
+      });
     })
     .catch(console.error);
 };
 
 main();
 
-// TODO Save JSON in file
 // TODO bug fix
 // TODO refactor
