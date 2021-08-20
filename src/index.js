d60008e5514fed1405c1686cbf5ad955becc8652
kal9isa <docalisa_it@yahoo.com>
2021-08-20 15:06:10 +0430

    fixed start day for month other than current

--- src/index.js
+++ src/index.js
@@ -1,8 +1,9 @@
 console.time();
 const axios = require('axios');
 const fs = require('fs');
-const { dayParser } = require('./services/day-parser');
 const { findEoM } = require('./services/find-eom');
+const { findSoM } = require('./services/find-som');
+const { dayParser } = require('./services/day-parser');
 const { monthParser } = require('./services/month-parser');
 
 // Get URL from user or use default
@@ -10,10 +11,6 @@ const weatherSrcURL =
   process.argv[2] != undefined
     ? process.argv[2]
     : 'https://www.accuweather.com/en/gb/london/ec4a-2/august-weather/328328';
-
-// Get current time to find day of month
-const today = new Date().getDate();
-// Can also find using "is-today" class on the elements after request
 
 // Wrap all requests in promise and collect data afterwards
 // Count of requests matches the result to equation below
@@ -49,11 +46,11 @@ const main = async () => {
       // Get a list of all daily forecast links related to this month
       let dayLinks = monthParser(response);
 
-      // Find last day of current month or EoM
-      let eom = findEoM(response);
-
       // Aggregate data from all daily forecast requests
-      let final = await sendAllReqs(dayLinks, eom - today);
+      let final = await sendAllReqs(
+        dayLinks,
+        findEoM(response) - findSoM(weatherSrcURL)
+      );
 
       fs.writeFile('daily-forecast.json', arrToJSON(final), (err) => {
         if (err) {
